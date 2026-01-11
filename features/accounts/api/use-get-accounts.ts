960453285c2137363de13a3544f603c9e9ae2381
Gonzalo Secades <gonzalo.secades@emerios.com>
2026-01-11 15:02:56 -0300

    feat: add custom hook for fetching accounts from API

---
+++ features/accounts/api/use-get-accounts.ts
@@ -0,0 +1,24 @@
+//hook to get account to getAccount endpoint
+
+import { useQuery } from '@tanstack/react-query';
+
+import { createApiError } from '@/lib/api-error';
+import { client } from '@/lib/hono';
+
+export const useGetAccounts = () => {
+  const query = useQuery({
+    queryKey: ['accounts'],
+    queryFn: async () => {
+      const response = await client.api.accounts.$get();
+
+      if (!response.ok) {
+        throw await createApiError(response, 'accounts');
+      }
+
+      const { data } = await response.json();
+      return data;
+    },
+  });
+
+  return query;
+};
