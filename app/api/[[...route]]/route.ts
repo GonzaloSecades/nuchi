f9234602461f6ac52c6ba8af033feb84d80042a6
GonzaloSecades <gsecades13@gmail.com>
2026-02-09 16:32:38 -0300

    Add summary api endpoint

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { handle } from 'hono/vercel';
 //route controllers
 import accounts from './accounts';
 import categories from './categories';
+import summary from './summary';
 import transactions from './transactions';
 
 //routes
@@ -15,7 +16,8 @@ const app = new Hono().basePath('/api');
 const routes = app
   .route('/accounts', accounts)
   .route('/categories', categories)
-  .route('/transactions', transactions);
+  .route('/transactions', transactions)
+  .route('/summary', summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
